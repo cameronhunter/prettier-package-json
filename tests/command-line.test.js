32d3a0e31b5074863ddab3cdc2b6595ce9570f2d
Jannik Zschiesche <hi+github@jannik.io>
2018-08-11 01:21:11 +0200

    Set exit code in -l command and cleanup tests (#9)
    * Set exit code in -l command and cleanup tests
    * Set exit code to 2 for different files
    * Add test for invalid JSON
    * Added correct testing of the JSON error case
    * Add docs about exit codes

--- tests/command-line.test.js
+++ tests/command-line.test.js
@@ -6,16 +6,12 @@ const fixture = (name) => path.relative(process.cwd(), path.join(__dirname, '__f
 
 const testCommand = (cmdline, cb) => {
   const [command, ...args] = cmdline.split(' ');
+  const matcher = cb
+      ? cb
+      : (result) => expect(result).toMatchSnapshot();
 
   test(`${command} ${args.join(' ')}`, () => {
-    !cb && expect.assertions(1);
-    return spawn(bin(command), args).then((result) => {
-      if (cb) {
-        cb(result);
-      } else {
-        expect(result).toMatchSnapshot();
-      }
-    });
+    return spawn(bin(command), args).then(matcher, matcher);
   });
 };
 
@@ -25,3 +21,9 @@ testCommand(`prettier-package-json --tab-width 8 ${fixture('package-1.json')}`);
 testCommand(`prettier-package-json ${fixture('package-*.json')}`);
 testCommand(`prettier-package-json --list-different ${fixture('package-*.json')}`);
 testCommand(`prettier-package-json --list-different ${fixture('missing.json')}`);
+testCommand(`prettier-package-json --list-different ${fixture('invalid.json')}`,
+  (result) => {
+    expect(result.exitCode).toBe(1);
+    expect(result.stdout).toBe("");
+    expect(result.stderr).toMatch(/tests\/__fixtures__\/invalid\.json: Unexpected token t in JSON at position 6/);
+});
