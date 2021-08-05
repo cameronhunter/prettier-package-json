a76d1d08a8011f79026e97b73d809667f065231d
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-08-05 22:40:03 +0530

    chore(tests): add test for prettify like scripts

--- tests/script-order.test.ts
+++ tests/script-order.test.ts
@@ -7,8 +7,25 @@ test('It orders scripts in alphabetical order, keeping pre and post scripts besi
       pretest: 'pretest',
       version: 'version',
       postversion: 'postversion',
-      build: 'build'
-    }
+      build: 'build',
+    },
+  };
+
+  expect(format(json)).toMatchSnapshot();
+});
+
+test('It sorts scripts beginning with pre or post in alphabetical order if their base script is not present', () => {
+  const json = {
+    scripts: {
+      prettify: 'prettify',
+      prepare: 'prepare',
+      parse: 'parse',
+      postprettify: 'postprettify',
+      prestart: 'prestart',
+      preprettify: 'preprettify',
+      start: 'start',
+      'postcss:watch': 'postcss:watch',
+    },
   };
 
   expect(format(json)).toMatchSnapshot();
