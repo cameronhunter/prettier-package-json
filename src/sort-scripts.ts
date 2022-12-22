657d7bc9e915e45b8d7537d1b83a3ccb35681b49
ofk <ofkjpn+github@gmail.com>
2022-12-22 10:52:20 +0900

    Fix sorting of script names to take into account separators (#65)

--- src/sort-scripts.ts
+++ src/sort-scripts.ts
@@ -3,15 +3,24 @@ import orderBy from 'sort-order';
 import { PackageJson } from './types';
 
 const PRE_OR_POST_PREFIX = /^(pre|post)/;
+const NPM_RUN_ALL_SEPARATOR = /([:/])/;
 
 // Sort alphabetically by script name excluding pre/post prefixes
 function scriptName(...args: [string, string]): 1 | 0 | -1 {
   const [a, b] = args.map((arg) => arg.replace(PRE_OR_POST_PREFIX, ''));
-  if (a !== b) {
-    return a < b ? -1 : 1;
-  } else {
-    return 0;
+  const aa = a.split(NPM_RUN_ALL_SEPARATOR);
+  const bb = b.split(NPM_RUN_ALL_SEPARATOR);
+  const al = aa.length;
+  const bl = bb.length;
+  for (let i = 0; i < al || i < bl; i++) {
+    if (aa[i] !== bb[i]) {
+      return aa[i] < bb[i] ? -1 : 1;
+    }
   }
+  if (al !== bl) {
+    return al < bl ? -1 : 1;
+  }
+  return 0;
 }
 
 // Sort by pre, script, post
--- tests/script-order.test.ts
+++ tests/script-order.test.ts
@@ -13,3 +13,20 @@ test('It orders scripts in alphabetical order, keeping pre and post scripts besi
 
   expect(format(json)).toMatchSnapshot();
 });
+
+test('It orders scripts with separators in alphabetical order', () => {
+  const json = {
+    scripts: {
+      dev: 'dev',
+      test: 'test',
+      lint: 'lint',
+      'lint:foo': 'lint:foo',
+      'lint:bar': 'lint:bar',
+      'lint-fix': 'lint-fix',
+      'lint-fix:baz': 'lint-fix:baz',
+      'lint-fix:qux': 'lint-fix:qux'
+    }
+  };
+
+  expect(format(json)).toMatchSnapshot();
+});
