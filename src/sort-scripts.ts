c748309083c0cf3ecbec5cc0d6c596ade8602381
ofk <ofkjpn+github@gmail.com>
2022-12-24 08:30:09 +0900

    Improve scripts sorting (#66)
    * Fix sorting of script names to take into account separators (#65)
    
    * Fix an issue that the order of scripts containing prefixes (e.g., prettier, postcss) is incorrect
    
    * Change prefix type
    
    * Rename variable names and write comments

--- src/sort-scripts.ts
+++ src/sort-scripts.ts
@@ -3,31 +3,90 @@ import orderBy from 'sort-order';
 import { PackageJson } from './types';
 
 const PRE_OR_POST_PREFIX = /^(pre|post)/;
+const NPM_RUN_ALL_SEPARATOR = /([:/])/;
+const NPM_BUILTIN_SCRIPTS = ['install', 'prepare', 'pack', 'publish', 'restart', 'start', 'stop', 'test', 'version', 'uninstall'];
+
+interface ParsedScriptName {
+  prefix?: 'pre' | 'post';
+
+  /**
+   * Base event name in the npm lifecycle. Often the string with prefix removed.
+   * For example, for an input `preprettier:lint`, the base would be `prettier:lint`.
+   */
+  base: string;
+
+
+  /**
+   * The full original name of the script
+   */
+  original: string;
+};
+
+function parseScriptName(scripts: PackageJson['scripts'], original: string): ParsedScriptName {
+  // prepublishOnly is a script whose base event is publish.
+  // cf. https://docs.npmjs.com/cli/v9/using-npm/scripts#npm-publish
+  if (original === 'prepublishOnly') {
+    return { prefix: 'pre', base: 'publish', original };
+  }
+
+  const prefixMatch = PRE_OR_POST_PREFIX.exec(original);
+  const prefix = prefixMatch ? prefixMatch[0] as 'pre' | 'post' : undefined;
+  const base = prefix ? original.slice(prefix.length) : original;
+
+  if (scripts?.hasOwnProperty(base) || NPM_BUILTIN_SCRIPTS.includes(base)) {
+    return { prefix, base, original };
+  }
+
+  return { prefix: undefined, base: original, original };
+}
 
 // Sort alphabetically by script name excluding pre/post prefixes
-function scriptName(...args: [string, string]): 1 | 0 | -1 {
-  const [a, b] = args.map((arg) => arg.replace(PRE_OR_POST_PREFIX, ''));
-  if (a !== b) {
-    return a < b ? -1 : 1;
-  } else {
+function scriptName(scripts: PackageJson['scripts']) {
+  return function (a: string, b: string): 1 | 0 | -1 {
+    const aParsed = parseScriptName(scripts, a);
+    const bParsed = parseScriptName(scripts, b);
+
+    // For example, for an input name `prettier:lint`, the segments would be `['prettier', ':', 'lint']`.
+    // The segments contains separators. There are two types of separators, to determine their order.
+    const aSegments = aParsed.base.split(NPM_RUN_ALL_SEPARATOR);
+    const bSegments = bParsed.base.split(NPM_RUN_ALL_SEPARATOR);
+
+    const aLength = aSegments.length;
+    const bLength = bSegments.length;
+
+    // Compare each segment, and when the strings are different, return 1 or -1 in alphabetical order.
+    for (let i = 0; i < Math.min(aLength, bLength); i++) {
+      if (aSegments[i] !== bSegments[i]) {
+        return aSegments[i] < bSegments[i] ? -1 : 1;
+      }
+    }
+
+    // Compare segments length, return 1 or -1 in ascending order of length.
+    if (aLength !== bLength) {
+      return aLength < bLength ? -1 : 1;
+    }
     return 0;
   }
 }
 
 // Sort by pre, script, post
-function prePostHooks(a: string, b: string): 1 | 0 | -1 {
-  if (a.startsWith('pre') || b.startsWith('post')) {
-    return -1;
-  } else if (a.startsWith('post') || b.startsWith('pre')) {
-    return 1;
-  } else {
+function prePostHooks(scripts: PackageJson['scripts']) {
+  return function (a: string, b: string): 1 | 0 | -1 {
+    const aParsed = parseScriptName(scripts, a);
+    const bParsed = parseScriptName(scripts, b);
+    if (aParsed.prefix === 'pre' || bParsed.prefix === 'post') {
+      return -1;
+    } else if (aParsed.prefix === 'post' || bParsed.prefix === 'pre') {
+      return 1;
+    } else if (aParsed.base !== bParsed.base) {
+      return aParsed.base < bParsed.base ? -1 : 1;
+    }
     return 0;
   }
 }
 
-const order = orderBy(scriptName, prePostHooks);
-
 export default function sortScripts(scripts: PackageJson['scripts']): { scripts?: PackageJson['scripts'] } {
   const keys = Object.keys(scripts || {}) as Array<keyof PackageJson['scripts']>;
+  const order = orderBy(scriptName(scripts), prePostHooks(scripts));
   return keys.length === 0 ? {} : { scripts: sortObject(scripts, keys.sort(order)) };
 }
--- tests/script-order.test.ts
+++ tests/script-order.test.ts
@@ -13,3 +13,44 @@ test('It orders scripts in alphabetical order, keeping pre and post scripts besi
 
   expect(format(json)).toMatchSnapshot();
 });
+
+test('It orders scripts with separators in alphabetical order', () => {
+  const json = {
+    scripts: {
+      dev: 'dev',
+      test: 'test',
+      lint: 'lint',
+      'lint:foo': 'lint:foo',
+      'lint:bar': 'lint:bar',
+      'lint-fix': 'lint-fix',
+      'lint-fix:baz': 'lint-fix:baz',
+      'lint-fix:qux': 'lint-fix:qux'
+    }
+  };
+
+  expect(format(json)).toMatchSnapshot();
+});
+
+test('It orders scripts that base script does not exist in alphabetical order', () => {
+  const json = {
+    scripts: {
+      lint: 'run-s lint:* *:lint',
+      'lint:eslint': 'lint:eslint',
+      'prettier:lint': 'prettier:lint',
+      'lint-fix': 'run-s lint-fix:* *:lint-fix',
+      'lint-fix:eslint': 'lint-fix:eslint',
+      'prettier:lint-fix': 'prettier:lint-fix',
+      postcss: 'postcss',
+      prepostcss: 'postpostcss',
+      postpostcss: 'postpostcss',
+      prepublishOnly: 'prepublishOnly',
+      prepare: 'prepare',
+      preprepare: 'preprepare',
+      postprepare: 'postprepare',
+      preinstall: 'preinstall',
+      postinstall: 'postinstall'
+    },
+  };
+
+  expect(format(json)).toMatchSnapshot();
+});
