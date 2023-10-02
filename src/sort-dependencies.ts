cd4b5916758ec8f7a3300060d2847f27379c7b72
Cameron Hunter <hello@cameronhunter.co.uk>
2023-10-02 16:12:23 -0700

    Revert "Fix sorting differences with npm deps (#68)"
    This reverts commit 61ef4354e4ef0c28b614c897b67c7ad304a0ce04.

--- src/sort-dependencies.ts
+++ src/sort-dependencies.ts
@@ -1,6 +1,10 @@
 import sortObject from 'sort-object-keys';
 import { PackageJson } from './types';
 
+function order(a: string, b: string) {
+  return a.localeCompare(b, 'en');
+}
+
 export default function sortDependencies<
   TKey extends
     | 'bundledDependencies'
@@ -12,5 +16,5 @@ export default function sortDependencies<
 >(key: TKey, packageJson: PackageJson) {
   const dependencies = packageJson[key];
   const keys = Object.keys(dependencies || {}) as Array<keyof typeof dependencies & string>;
-  return keys.length === 0 ? {} : { [key]: sortObject(dependencies, keys.sort()) };
+  return keys.length === 0 ? {} : { [key]: sortObject(dependencies, keys.sort(order)) };
 }
